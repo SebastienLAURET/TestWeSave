b95fdd27c466f0a079fa313f5566b777b8d578fd
lauret_s <lauret_s@epitech.eu>
2018-02-17 15:04:59 +0100

    require new file

--- backend/level5/drivy/lib/drivy.rb
+++ backend/level5/drivy/lib/drivy.rb
@@ -6,5 +6,6 @@ require_relative 'drivy/car/car'
 require_relative 'drivy/rent/rent'
 require_relative 'drivy/rent/commission'
 require_relative 'drivy/rent/action'
+require_relative 'drivy/rent/options'
 
 require_relative 'drivy/main'
