4a8654786a0cbf67b57081790c2078fbeb1f5a15
lauret_s <lauret_s@epitech.eu>
2018-02-17 15:03:15 +0100

    create function to create different actions

--- backend/level5/drivy/lib/drivy/rent/action.rb
+++ backend/level5/drivy/lib/drivy/rent/action.rb
@@ -11,6 +11,31 @@ module Drivy
       DEBIT = 'debit'.freeze
       CREDIT = 'credit'.freeze
 
+      def self.generate_driver_action(rent)
+        amount = rent.calculate_price_with_reduction
+        Action.new Action::DRIVER, Action::DEBIT, amount
+      end
+
+      def self.generate_owner_action(rent)
+        amount = rent.calculate_price_without_commission
+        Action.new Action::OWNER, Action::CREDIT, amount
+      end
+
+      def self.generate_insurance_action(rent)
+        amount = rent.calculate_commission.insurance_fee
+        Action.new Action::INSURANCE, Action::CREDIT, amount
+      end
+
+      def self.generate_assistance_action(rent)
+        amount = rent.calculate_commission.assistance_fee
+        Action.new Action::ASSISTANCE, Action::CREDIT, amount
+      end
+
+      def self.generate_drivy_action(rent)
+        amount = rent.calculate_commission.drivy_fee
+        Action.new Action::DRIVY, Action::CREDIT, amount
+      end
+
       attr_accessor :who, :type, :amount
 
       def initialize(who, type, amount)
